2c30b379fd81f0a7a7614299b30a9cf235695de9
Adriano <adrianotakara07@gmail.com>
2024-08-22 11:50:04 -0300

    Something

---
+++ Home.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.GridBagConstraints;
+
+public class Home{
+  public static JFrame frame=new JFrame("AAA");
+  public static JPanel homePage=new JPanel();
+  public static JPanel loginPage=new JPanel();
+  public static Register registerPage=new Register();
+
+  public static enum Page{
+    HOME,
+    REGISTER,
+    LOGIN,
+  };
+
+  public static Page currentPage=Page.HOME;
+
+  public static void goToPage(Page toPage){
+    currentPage=toPage;
+
+    if(toPage==Page.HOME) frame.setContentPane(homePage);
+    else if(toPage==Page.LOGIN) frame.setContentPane(loginPage);
+    else if(toPage==Page.REGISTER) frame.setContentPane(registerPage);
+
+    frame.revalidate();
+  }
+
+  public static void main(String[] args){
+    homePage.setLayout(new GridBagLayout());
+    
+    Button registerButton=new Button("Register");
+    Button loginButton=new Button("Login");
+
+    registerButton.addActionListener(e->{
+      goToPage(Page.REGISTER);
+    });
+
+    loginButton.addActionListener(e->{
+      goToPage(Page.LOGIN);
+    });
+
+    GridBagConstraints gbc=new GridBagConstraints();
+
+    gbc.gridy=0;
+    gbc.fill=1;
+    gbc.insets=new Insets(0,0,75,0);
+    homePage.add(registerButton,gbc);
+
+    gbc.gridy=1;
+    gbc.fill=1;
+    gbc.insets=new Insets(0,0,0,0);
+    homePage.add(loginButton,gbc);
+
+    frame.add(homePage);
+    // frame.add(registerPanel);
+    frame.setSize(800,800);
+    frame.setLocationRelativeTo(null);
+    frame.setLayout(new GridBagLayout());
+    frame.setVisible(true);
+  }
+}
