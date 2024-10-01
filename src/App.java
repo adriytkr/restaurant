d16ab77096b0bf7fb3d57544f422a60db4eb4e17
João Souto <joaosouto0@outlook.com>
2024-10-01 01:04:04 -0300

    PedidosDAO finalizados, falta de tratativa de exceções

--- src/App.java
+++ src/App.java
@@ -6,10 +6,6 @@ import src.classes.*;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        Pedidos pedidoTeste = new Pedidos(0, 0, 0, 0, 0, null, null);
-        pedidoTeste.setDataPedido("2024-09-29 00:07:06");
-        pedidoTeste.setIdCliente(3);
-        pedidoTeste = PedidosDAO.consultarPedido(pedidoTeste);
-        pedidoTeste.visualizarPedidos();
+
     }
 }
